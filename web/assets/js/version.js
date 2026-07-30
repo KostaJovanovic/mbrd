8c2ebfd47f3ab4404f90267887bbbccfaab25cf3
Kosta Jovanovic <Kosta.Jovanovic@gmail.com>
2026-07-30 04:01:24 +0200

    v0.75

--- web/assets/js/version.js
+++ web/assets/js/version.js
@@ -1,6 +1,6 @@
 ﻿// Stamped by save.bat on every commit - do not edit these two lines by hand.
-export const COMMIT_COUNT = 70;
-export const VERSION = '0.70';
+export const COMMIT_COUNT = 75;
+export const VERSION = '0.75';
 
 // Commits crowned as major releases. Keep in sync with RELEASES in save.bat.
 export const RELEASE_COMMITS = [];
