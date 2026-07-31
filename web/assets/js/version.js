582b922174544d54fc192203ce6a4b99f892f911
Kosta <valjdakosta@gmail.com>
2026-07-31 22:45:25 +0200

    v0.105

--- web/assets/js/version.js
+++ web/assets/js/version.js
@@ -1,6 +1,6 @@
 ﻿// Stamped by save.bat on every commit - do not edit these two lines by hand.
-export const COMMIT_COUNT = 92;
-export const VERSION = '0.92';
+export const COMMIT_COUNT = 105;
+export const VERSION = '0.105';
 
 // Commits crowned as major releases. Keep in sync with RELEASES in save.bat.
 export const RELEASE_COMMITS = [];
