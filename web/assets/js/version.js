d8957cca0ea5f0b4ad0cfcae19fe9132781dd447
mjova <miljan.jovanovic@flatsoft.co.rs>
2026-08-16 04:40:13 +0300

    v0.233

--- web/assets/js/version.js
+++ web/assets/js/version.js
@@ -1,6 +1,6 @@
 ﻿// Stamped by save.bat on every commit - do not edit these two lines by hand.
-export const COMMIT_COUNT = 231;
-export const VERSION = '0.231';
+export const COMMIT_COUNT = 233;
+export const VERSION = '0.233';
 
 // Commits crowned as major releases. Keep in sync with RELEASES in save.bat.
 export const RELEASE_COMMITS = [];
